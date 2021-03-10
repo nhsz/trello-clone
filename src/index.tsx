ae8f61dc1da47a7f2e786014214f9796b9ba8718
nhsz <nh.quiroz@gmail.com>
2021-03-10 18:13:00 -0300

    chore: add DnD provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { StrictMode } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import ReactDOM from 'react-dom';
 import { App } from './components';
 import { AppStateProvider } from './contexts';
@@ -7,9 +9,11 @@ import './index.css';
 
 ReactDOM.render(
   <StrictMode>
-    <AppStateProvider>
-      <App />
-    </AppStateProvider>
+    <DndProvider backend={HTML5Backend}>
+      <AppStateProvider>
+        <App />
+      </AppStateProvider>
+    </DndProvider>
   </StrictMode>,
   document.getElementById('root')
 );
